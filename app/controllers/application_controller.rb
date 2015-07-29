dad50a79b1b92d7d6c719969d0e6631729e30f6f
libochao <522427432@qq.com>
2015-07-29 15:39:13 +0800

    commit part of the international code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :authorize
+  before_filter :set_i18n_locale_from_params
+
   protect_from_forgery
 
   private
@@ -16,6 +18,21 @@ class ApplicationController < ActionController::Base
       #...
 
     protected
+      def set_i18n_locale_from_params
+        if params[:locale]
+          if I18n.available_locales.include?(params[:locale].to_sym)
+            I18n.locale = params[:locale]
+          else
+            flash.now[:notice] = 
+              "#{params[:locale]} translation not available"
+            logger.error flash.now[:notice]
+          end
+        end
+      end
+
+      def default_url_options
+        { :locale => I18n.locale}
+      end
 
       def authorize
         unless User.find_by_id(session[:user_id])
